a81a58239391b4f617a26c8c2b7b6b91625dd73b
FRC-1250 <hfarobotics@hfa-dearborn.org>
2018-01-25 23:50:43 -0500

    Initial Drivetrain Subsystem Setup

--- src/org/usfirst/frc/team1250/robot/subsystems/Sub_DriveTrain.java
+++ src/org/usfirst/frc/team1250/robot/subsystems/Sub_DriveTrain.java
@@ -1,18 +1,40 @@
 package org.usfirst.frc.team1250.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team1250.robot.RobotMap;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.*;
 /**
  *
  */
 public class Sub_DriveTrain extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+	WPI_TalonSRX fLeftMotor = new WPI_TalonSRX( RobotMap.DRV_LEFT_FRONT);
+	WPI_TalonSRX bLeftMotor = new WPI_TalonSRX( RobotMap.DRV_LEFT_BACK);
+	WPI_TalonSRX mLeftMotor = new WPI_TalonSRX( RobotMap.DRV_LEFT_MID);
+	WPI_TalonSRX bRightMotor = new WPI_TalonSRX( RobotMap.DRV_RIGHT_BACK);
+	WPI_TalonSRX fRightMotor = new WPI_TalonSRX( RobotMap.DRV_RIGHT_FRONT);
+	WPI_TalonSRX mRightMotor = new WPI_TalonSRX( RobotMap.DRV_RIGHT_MID);
+	
+	private SpeedController m_leftMotor
+	= new SpeedControllerGroup(fLeftMotor, bLeftMotor, mLeftMotor);
+	
+	private SpeedController m_rightMotor
+	= new SpeedControllerGroup(fRightMotor, bRightMotor, mRightMotor);
+	
+	private DifferentialDrive m_drive
+	= new DifferentialDrive(m_leftMotor, m_rightMotor);
+	
+	// High and Low RPM thresholds for shifting
+	private final double THRESH_RPM_HI= 0;
+	private final double THRESH_RPM_LO = 0;
+    
+	public void initDefaultCommand() {
+		
+    	
     }
 }
 

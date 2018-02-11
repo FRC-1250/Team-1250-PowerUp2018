2ade3221e438b99adaba5abb15d929e53905361a
Rodney Lewis <rodneyll@umich.edu>
2018-02-10 23:29:15 -0500

    Intake functions
    - Split full collect into two separate CGs, Herd and Eat
    - Added ele pinch toggle for manual use
    - Removed some functions

---
+++ src/org/usfirst/frc/team1250/robot/intake/Cmd_IntakeCollect.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team1250.robot.intake;
+
+import org.usfirst.frc.team1250.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class Cmd_IntakeCollect extends Command {
+
+    public Cmd_IntakeCollect() {
+    	requires(Robot.s_intake);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+		Robot.s_intake.collect();
+    }
+
+    protected boolean isFinished() {
+		return (!Robot.s_intake.isSeenRight() && !Robot.s_intake.isSeenLeft());
+    }
+
+    protected void end() {
+    	Robot.s_intake.stop();
+    }
+
+    protected void interrupted() {
+    }
+}

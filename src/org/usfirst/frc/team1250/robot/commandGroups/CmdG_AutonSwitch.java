17d32f1e90388cd3fbba492c3fc8afee29ae729d
NotDriver <32750071+RincewindBoi@users.noreply.github.com>
2018-02-20 17:08:55 -0500

    Switch Update

--- src/org/usfirst/frc/team1250/robot/commandGroups/CmdG_AutonSwitch.java
+++ src/org/usfirst/frc/team1250/robot/commandGroups/CmdG_AutonSwitch.java
@@ -14,18 +14,21 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class CmdG_AutonSwitch extends CommandGroup {
 
-    public CmdG_AutonSwitch() {
+	// Pass 1 for Right
+	// Pass -1 for Left
+		
+    public CmdG_AutonSwitch(int dir) {
     	 addSequential(new Cmd_Reset());
     	 addParallel(new Cmd_EleSwitch());
     	 addSequential(new Cmd_AutoDrive(48, .6 , .3));
     	 addSequential(new Cmd_DoNothing(.1));
     	 addSequential(new Cmd_Reset());
-    	 addSequential(new Cmd_AutoTurn(-45 , .8 , .7));  
+    	 addSequential(new Cmd_AutoTurn(45 * dir, .8 , .7));  
     	 addSequential(new Cmd_Reset());
     	 addSequential(new Cmd_AutoDrive(72, .6 , .3));
     	 addSequential(new Cmd_DoNothing(.1));
     	 addSequential(new Cmd_Reset());
-    	 addSequential(new Cmd_AutoTurn(45, .8 , .7)); 
+    	 addSequential(new Cmd_AutoTurn(-45 * dir, .8 , .7)); 
     	 addSequential(new Cmd_Reset());
     	 addSequential(new Cmd_AutoDrive(6, .6 , .3));
     	 addSequential(new Cmd_Reset());
--- src/org/usfirst/frc/team1250/robot/groups/Auto_CenterPos.java
+++ src/org/usfirst/frc/team1250/robot/groups/Auto_CenterPos.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1250.robot.groups;
 
 import org.usfirst.frc.team1250.robot.Robot;
+import org.usfirst.frc.team1250.robot.commandGroups.CmdG_AutonSwitch;
 import org.usfirst.frc.team1250.robot.drive.Cmd_AutoDrive;
 import org.usfirst.frc.team1250.robot.elevator.Cmd_EleErectTower;
 
@@ -18,15 +19,15 @@ public class Auto_CenterPos extends CommandGroup {
     	    	
      	if(autoMessage == "LR" || autoMessage =="LL")
     	{
-    		//Right Switch case
-//     		addSequential(new Cmd_AutoDrive(60));
+    		//Left Switch case
+    		addSequential(new CmdG_AutonSwitch(-1));
      		//addSequential(new Cmd_AutoTurn(30));
      		//addSequential(new Cmd_AutoDrive(45));
     	}
     	else if(autoMessage == "RL" || autoMessage == "RR")
     	{
-    		//Left Switch case
-//    		addSequential(new Cmd_AutoDrive(60));
+    		//Right Switch case
+   		addSequential(new CmdG_AutonSwitch(1));
      		//addSequential(new Cmd_AutoTurn(-30));
      		//addSequential(new Cmd_AutoDrive(45));
     	}
@@ -35,21 +36,6 @@ public class Auto_CenterPos extends CommandGroup {
     		//Do nothing else
     	}
     	
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
     }
 }
